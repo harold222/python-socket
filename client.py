d63594f59746b8869a53a99ec8836e0c48e97986
Harold Pedraza <32584758+harold222@users.noreply.github.com>
2022-02-27 18:19:15 -0500

    client

---
+++ client.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+
+username = input("Ingrese su usuario: ")
+
+host = '127.0.0.1'
+port = 55555
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+
+
+def receiveMessages():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+
+            if message == "@username":
+                client.send(username.encode("utf-8"))
+            else:
+                print(message)
+        except:
+            print("An error Ocurred")
+            client.close()
+            break
+
+
+def writeMessages():
+    while True:
+        try:
+            message = f"    {username}: {input('')}"
+            client.send(message.encode('utf-8'))
+        except:
+            break
+
+
+# creo dos hilos para cada funcion
+receive_thread = threading.Thread(target=receiveMessages)
+receive_thread.start()
+
+write_thread = threading.Thread(target=writeMessages)
+write_thread.start()
--- server.py
+++ server.py
@@ -19,9 +19,9 @@ usernames = []
 
 
 # Enviara mensaje a todos los clientes
-def broadcast(message, currentClient):
+def broadcast(message, _client):
     for client in clients:
-        if client != currentClient:
+        if client != _client:
             client.send(message)
 
 
@@ -35,30 +35,40 @@ def handleMessages(client):
             index = clients.index(client)
             username = usernames[index]
             # utf-8 para la codificacion
-            broadcast(f"chat: {username} desconectado.".encode('utf-8'))
+            broadcast(f"CHAT: {username} se ha desconectado.".encode('utf-8'), client)
 
             # Elimino al cliente
             clients.remove(client)
             usernames.remove(username)
             client.close()
+            break
+
 
 
 def receiveConnections():
-    # acepta conexiones de los clientes
-    # retorna objeto conexion cliente y ip y port de la conexion del cliente
-    client, address = server.accept()
+    while True:
+        # acepta conexiones de los clientes
+        # retorna objeto conexion cliente y ip y port de la conexion del cliente
+        client, address = server.accept()
 
-    # el servidor requiere el username del cliente
-    client.send("@username".encode('utf-8'))
+        # el servidor requiere el username del cliente
+        client.send("@username".encode("utf-8"))
 
-    # decodifico el mensaje
-    username = client.recv(1024).decode('utf-8')
+        # decodifico el mensaje
+        username = client.recv(1024).decode('utf-8')
 
-    clients.append(client)
-    usernames.append(username)
+        clients.append(client)
+        usernames.append(username)
 
-    print(f"el usuario: {username} con ip {str(address)} esta conectado.")
+        print(f"El usuario: {username} esta conectado con {str(address)}")
 
-    message = f"Chat: {username} joined the chat".encode('utf-8')
-    broadcast(message, client)
-    client.send('Conectado al servidor'.encode('utf8'))
+        message = f"CHAT: {username} ingreso al chat".encode("utf-8")
+        broadcast(message, client)
+        client.send('Conectado al servidor'.encode("utf8"))
+
+        # por cada cliente que se conecte se asignara un hilo para tener mensajes por separado
+        thread = threading.Thread(target=handleMessages, args=(client,))
+        thread.start()
+
+
+receiveConnections()
